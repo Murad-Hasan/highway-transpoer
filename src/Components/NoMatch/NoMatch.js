2c879a950a593c003f24f136380399af66a22882
Murad-Hasan <muradhasan.7845@gmail.com>
2021-03-20 22:13:01 +0600

    complete project

--- src/Components/NoMatch/NoMatch.js
+++ src/Components/NoMatch/NoMatch.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NoMatch = () => {
     return (
         <div>
-            <h2 className='text-center'>Page not found</h2>
+            <h2 className='text-center'>Page not found</h2> 
         </div>
     );
 };
